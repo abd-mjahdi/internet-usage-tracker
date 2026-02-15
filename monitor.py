85d462ff3518c9891ef4c8f27275f4f35008b6e2
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-15 02:20:24 +0100

    added function so that for every connection i store the source port and dest port in both ways

--- monitor.py
+++ monitor.py
@@ -10,3 +10,16 @@ def get_all_network_adapter_macs():
     all_mac_addresses = {iface.mac for iface in ifaces.values()}
     return all_mac_addresses
 
+def track_connections():
+    for con in psutil.net_connections():
+        try:
+            local_port = con.laddr.port
+            remote_port = con.raddr.port
+            con_pid = con.pid 
+        except Exception:
+            continue
+
+        connection2pid[( local_port , remote_port )]=con_pid    
+        connection2pid[( remote_port , local_port )]=con_pid
+
+
